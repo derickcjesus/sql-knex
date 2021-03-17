d4e451bbdb20ba40194f9ea19e96c67a406ece74
Derick de Jesus <derickcjesus@gmail.com>
2021-03-17 17:16:19 -0300

    finishes CRUD

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const routes = require('./routes')
 
 const app = express();
+
+app.use(express.json())
 app.use(routes)
+
+//catch all
+app.use( (error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({error: error.message})
+})
 
 app.listen(3331, () => console.log("server is runnig"));
