5a402ea86f68a41ff557a22e453447ed21e30a08
Derick de Jesus <derickcjesus@gmail.com>
2021-03-19 11:33:22 -0300

    pagination

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const routes = express.Router()
+const express = require("express");
+const routes = express.Router();
 
-const UserController = require('./controllers/UserController')
+const UserController = require("./controllers/UserController");
+const ProjectController = require("./controllers/ProjectController");
 
-routes.get('/users', UserController.index)
-routes.post('/users', UserController.create)
-routes.put('/users/:id', UserController.update)
-routes.delete('/users/:id', UserController.delete)
+routes
+  //rotas users
+  .get("/users", UserController.index)
+  .post("/users", UserController.create)
+  .put("/users/:id", UserController.update)
+  .delete("/users/:id", UserController.delete);
 
-module.exports = routes
+//rotas projects
+routes
+  .get("/projects", ProjectController.index)
+  .post("/projects", ProjectController.create);
+
+module.exports = routes;
